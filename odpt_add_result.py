788f60e68f47e022ec5af37897586a3b32ce70ed
Maxim Uvarov <maxim.uvarov@linaro.org>
2017-12-18 23:20:51 +0300

    initial commit
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

---
+++ odpt_add_result.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+import json
+import pickle
+import sys
+
+def usage():
+	print "Script to add test result in odpt_results.dat"
+	print "Usage %s test_name field value" % sys.argv[0]
+	print "Example: %s generator RX 1000" % sys.argv[0]
+
+if len(sys.argv) != 4:
+	usage()
+	sys.exit(-1)
+
+name = sys.argv[1]
+result = sys.argv[2]
+value = sys.argv[3]
+
+fname = "odpt_results.dat"
+
+try:
+	data = pickle.load( open(fname, "rb" ) )
+except:
+	data = {}
+
+if data[name]:
+	tmp = data[name]
+	tmp[result] = value
+	data[name] =  tmp
+else:
+	data[name] = {{result :value }}
+
+json_data = json.dumps(data)
+
+pickle.dump(data, open(fname, "w" ))
+
+print json_data
---
+++ odpt_post_results.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import json
+import pickle
+import sys
+import urllib2
+import datetime
+
+url = sys.argv[1]
+author = sys.argv[2]
+git_commit = sys.argv[3]
+
+fname = "odpt_results.dat"
+
+def get_utc_time():
+	d = datetime.datetime.utcnow()
+	epoch = datetime.datetime(1970,1,1)
+	t = (d - epoch).total_seconds()
+	return t
+
+d = pickle.load( open(fname, "rb" ) )
+data = {"time" : get_utc_time(),
+	"commit" : git_commit,
+	"author" : author,
+	"results" : d }
+
+
+req = urllib2.Request(url)
+req.add_header('Content-Type','application/json')
+
+js = json.dumps(data)
+
+print urllib2.urlopen(req, js).read()
